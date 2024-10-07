66a97761b821d41e67d40793f8e41a732ac18c66
Muhammed Abdrahman <muhammed101amin@gmail.com>
2024-10-08 03:39:08 +0500

    feat: implement cli subtasks

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
-const parseEnv = () => {
-    // Write your code here 
+export const parseEnv = () => {
+    const envVars = [];
+    for (let envVar in process.env) {
+        if (envVar.startsWith("RSS")) {
+            const string = `${envVar}=${process.env[envVar]}`;
+            envVars.push(string);
+        }
+    }
+
+    const result = envVars.join("; ");
+    console.log(result);
 };
 
-parseEnv();
+parseEnv()

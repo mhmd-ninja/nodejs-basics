4c9fa1d03ca95d4f2cb8485e43eda29f94a9f08c
Muhammed Abdrahman <muhammed101amin@gmail.com>
2024-10-08 03:53:16 +0500

    feat: implement zip subtasks

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
-const decompress = async () => {
-    // Write your code here 
+import { createReadStream, createWriteStream } from "fs";
+import { dirname, join } from "path";
+import { pipeline } from "stream";
+import { fileURLToPath } from "url";
+import { createUnzip } from "zlib";
+
+const dirPath = fileURLToPath(import.meta.url);
+const dirName = dirname(dirPath);
+const originalFilePath = join(dirName, "files", "archive.gz");
+const restoredFilePath = join(dirName, "files", "fileToCompress.txt");
+
+export const decompress = async () => {
+    const unzip = createUnzip();
+    const originalFile = createReadStream(originalFilePath);
+    const restoredFile = createWriteStream(restoredFilePath);
+
+    pipeline(originalFile, unzip, restoredFile, (error) => {
+        if (error) {
+            console.log(error);
+            process.exitCode = 1;
+        }
+    });
 };
 
 await decompress();

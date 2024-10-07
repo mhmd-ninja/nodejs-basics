061c5a563e00ae4ffb33664bfe0cd9ef9b58c553
Muhammed Abdrahman <muhammed101amin@gmail.com>
2024-10-08 03:35:24 +0500

    feat: implement fs subtasks

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,27 @@
-const list = async () => {
-    // Write your code here 
+import { open } from "fs";
+import { readdir, stat } from "fs/promises";
+import { dirname, join, parse } from "path";
+import { fileURLToPath } from "url";
+
+const filePath = fileURLToPath(import.meta.url);
+const dirName = dirname(filePath);
+const filesDirPath = join(dirName, "files");
+
+const isFile = (path) => stat(path).then(() => true).catch(() => false);
+
+export const list = async () => {
+    open(filesDirPath, async (err) => {
+        if (err) {
+            throw new Error("FS Operation failed");
+        }
+
+        const files = await readdir(filesDirPath, { withFilesType: true });
+        for (let file of files) {
+            if (await isFile(join(filesDirPath, file))) {
+                console.log(parse(file).name);
+            }
+        }
+    });
 };
 
 await list();

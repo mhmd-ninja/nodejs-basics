dd94a551e4389755ff0c5221e1bae0daf666fa45
Muhammed Abdrahman <muhammed101amin@gmail.com>
2024-10-08 04:02:24 +0500

    feat: implement cp subtask

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,24 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { spawn } from "child_process";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const dirPath = fileURLToPath(import.meta.url);
+const dirName = dirname(dirPath);
+const modulePath = join(dirName, "files", "script.js");
+
+export const spawnChildProcess = async (args) => {
+    const child = spawn("node", [modulePath, ...args]);
+
+    process.stdin.pipe(child.stdin);
+
+    child.stdout.on("data", (data) => {
+        console.log(data.toString());
+    });
+
+    child.on("error", (error) => {
+        console.log(error);
+        process.exit(1);
+    });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["arg1", "arg2", "arg3"]);
